7fbecfed7e14aea7d29f1f93d1d8c45f2abfce34
Pranto Kumar <40034555+prantok104@users.noreply.github.com>
2024-04-16 01:20:37 +0600

    single news page design start

--- pages/news/[slug].jsx
+++ pages/news/[slug].jsx
@@ -3,10 +3,14 @@ const SingleNews = () => {
    return (
       <div className="single-news-content">
           <ImageTitle title="Read Full News" />
-          <div className="single-news-area">
+          <div className="single-news-area py-5">
             <div className="container">
                <div className="row">
                      <div className="col-md-9">
+                        single news content
+                     </div>
+                     <div className="col-md-3">
+                        sidebar
                      </div>
                   </div>
                </div>
