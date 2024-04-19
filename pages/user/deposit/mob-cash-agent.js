27bd71d5bf7d833792663c72f8b19c5b4d60ff6e
Pranto Kumar <40034555+prantok104@users.noreply.github.com>
2024-04-20 00:25:42 +0600

    agent deposit design area completed

---
+++ pages/user/deposit/mob-cash-agent.js
@@ -0,0 +1,86 @@
+import AlertCard from '@/components/AlertCard';
+import Card from '@/components/Card';
+import { HttpClientCall } from '@/components/HTTPClient';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { Form } from 'react-bootstrap';
+const mobCashAgent = () => {
+   const [search, setSearch] = useState('');
+   const [agents, setAgents] = useState([
+     {
+       id: 130,
+       identity: "124571",
+       address: "Guabari, Borobihanali-6250, Bagmara, Rajshahi",
+       telegram_link: "t.me/prantomoly",
+     },
+     {
+       id: 195,
+       identity: "124588",
+       address: "gazipur sadar kapasia bazar",
+       telegram_link: "Marly102",
+     },
+     {
+       id: 196,
+       identity: "124589",
+       address: "sylhet",
+       telegram_link: "t.me/Bob_bp",
+     },
+     {
+       id: 202,
+       identity: "124595",
+       address: "adf",
+       telegram_link: "ad",
+     },
+   ]);
+   useEffect(() => {
+      if(search?.length > 3){
+          async function fetchAgents() {
+            const agents = await HttpClientCall({
+              method: "GET",
+              endpoint: `user/deposit/mob-cash/agent?admin_id=${search}`, // Change the endpoint its just demo purpose
+              includeAuth: true,
+              data: [],
+            });
+
+            if (agents) {
+              setAgents(agents);
+            }
+          }
+          fetchAgents();
+      }
+   }, [search])
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-9 mx-auto">
+          <Card header="Mob Cash agent in your region">
+            <AlertCard message="Recomanded Mob/Cash agents" />
+            <div className="mt-3">
+              <Form.Control
+                type="text"
+                name="search"
+                placeholder="Search Agent by Agent ID"
+                onChange={(e) => setSearch(e?.target?.value)}
+              />
+
+              <div className="show-agents-list">
+                {agents?.map((item, index) => (
+                  <Link
+                    className="single-agent-for-deposit"
+                    key={`mob-agent${index}`}
+                    href={`https://${item?.telegram_link}`}
+                  >
+                    <span>Agent: {item?.identity}</span>
+                    <span>address: {item?.address}</span>
+                  </Link>
+                ))}
+              </div>
+            </div>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default mobCashAgent
