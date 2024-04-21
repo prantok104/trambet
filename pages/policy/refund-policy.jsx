a30c3cbdb0b7e5851ce00ceaa556033c567b226e
Hashibul Hasan <hashibul1914@gmail.com>
2024-04-21 20:18:38 +0600

    All policy Implemented

--- pages/policy/refund-policy.jsx
+++ pages/policy/refund-policy.jsx
@@ -1,63 +1,34 @@
 import React from 'react'
 import ImageTitle from '@/components/ImageTitle'
+import { useEffect, useState } from "react";
+import { getRefundPolicy} from "@/services/common";
 const refundPolicy = () => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [refundPolicyData, setRefundPolicyData] = useState([]);
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getRefundPolicy();
+      console.log("Fetched data:", data); // Log the fetched data
+      setRefundPolicyData(data);
+    }
+    fetchData();
+  }, []);
   return (
     <div class="policy-page-content">
-      <ImageTitle title="Refund Policy" />
+      <ImageTitle title={refundPolicyData.title} />
       <div className="p-5">
         <div className="mb-5">
           <p>
            Sure, here is the refund policy for a sports betting platform in terms of sports betting:
           </p>
         </div>
-        
-        <div className="mb-5">
-          <h2>Refund Policy</h2>
-          <h4>1. Purpose</h4>
-          <p>
-           This refund policy outlines the circumstances under which our sports betting platform will issue a refund to a customer.
-          </p>
-        </div>
-        <div className="mb-5">
-          <h2>Refund Policy</h2>
-          <ul>
-            <li>
-              Configuration requests - If you have a fully managed dedicated
-              server with us then we offer custom PHP/MySQL configurations,
-              firewalls for dedicated IPs, DNS, and httpd configurations.
-            </li>
-            <li>
-              Software requests - Cpanel Extension Installation will be granted
-              as long as it does not interfere with the security, stability, and
-              performance of other users on the server.
-            </li>
-            <li>
-              Emergency Support - We do not provide emergency support / Phone
-              Support / LiveChat Support. Support may take some hours sometimes.
-            </li>
-            <li>
-              Webmaster help - We do not offer any support for webmaster related
-              issues and difficulty including coding, & installs, Error solving.
-              if there is an issue where a library or configuration of the
-              server then we can help you if it's possible from our end.
-            </li>
-            <li>
-              Backups - We keep backups but we are not responsible for data
-              loss, you are fully responsible for all backups.
-            </li>
-          </ul>
-        </div>
-        <div className="mb-5">
-          <h2>What information do we collect?</h2>
-          <p>
-            We gather data from you when you register on our site, submit a
-            request, buy any services, react to an overview, or round out a
-            structure. At the point when requesting any assistance or enrolling
-            on our site, as suitable, you might be approached to enter your:
-            name, email address, or telephone number. You may, nonetheless,
-            visit our site anonymously.
-          </p>
-        </div>
+
+          <div className="mb-5">
+              <h2>{refundPolicyData.title}</h2>
+              <p dangerouslySetInnerHTML={{__html: refundPolicyData.details}}/>
+          </div>
+
       </div>
     </div>
   );
---
+++ services/common.jsx
@@ -0,0 +1,49 @@
+import { HttpClientCall } from "@/components/HTTPClient";
+
+
+export async  function getPrivacyPolicy() {
+    return await HttpClientCall({
+        endpoint: "get-privacy-policy",
+        method: "GET",
+        includeAuth: false,
+        data: [],
+    })
+        .then((response) => {
+            return response.data;
+
+        })
+        .catch((error) => {
+            return [];
+        });
+}
+
+export async  function getTermsOfService() {
+    return await HttpClientCall({
+        endpoint: "terms-of-service",
+        method: "GET",
+        includeAuth: false,
+        data: [],
+    })
+        .then((response) => {
+            return response.data;
+
+        })
+        .catch((error) => {
+            return [];
+        });
+}
+export async  function getRefundPolicy() {
+    return await HttpClientCall({
+        endpoint: "refund-policy",
+        method: "GET",
+        includeAuth: false,
+        data: [],
+    })
+        .then((response) => {
+            return response.data;
+
+        })
+        .catch((error) => {
+            return [];
+        });
+}
