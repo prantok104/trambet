1dbd28431b8f50252a0a5bcedd1fd57b3efc9980
Hashibul Hasan <hashibul1914@gmail.com>
2024-04-21 19:03:45 +0600

    News Details Get API Added

--- pages/news/index.jsx
+++ pages/news/index.jsx
@@ -6,7 +6,6 @@ import ConstantData from '@/components/ConstantData';
 
 export default function NewsPage() {
   const [newsData, setNewsData] = useState([]);
-  const baseUrl = ConstantData.API_BASE_URL;
   useEffect(() => {
     async function fetchData() {
       const data = await getAllNews();
@@ -21,19 +20,21 @@ export default function NewsPage() {
       <div className="all-news  py-5">
         <div className="container">
           <div className="row">
-            <div className="col-md-4">
               {newsData?.map((newsItem, index) => (
-                  <NewsCard key={index}
-                            image={`${baseUrl}/newsItem.image`}
-                            title={newsItem.title}
-                            content={newsItem.details}
-                            link={`news/${newsItem.id}`}
+                  // eslint-disable-next-line react/jsx-key
+                      <div className="col-md-4">
+                        <NewsCard key={index}
+                                  image={newsItem.image}
+                                  title={newsItem.title}
+                                  content={newsItem.details}
+                                  link={`news/${newsItem.id}`}
 
-                  />
-              ))}
-            </div>
-          </div>
-        </div>
+                        />
+                      </div>
+                    ))}
+
+                </div>
+                </div>
       </div>
     </div>
   );
--- services/news.jsx
+++ services/news.jsx
@@ -2,7 +2,7 @@ import { HttpClientCall } from "@/components/HTTPClient";
 
 
 export async  function getAllNews() {
-    await HttpClientCall({
+    return await HttpClientCall({
       endpoint: "news/1",
       method: "GET",
       includeAuth: false,
@@ -16,3 +16,19 @@ export async  function getAllNews() {
         return [];
       });
   }
+
+export async  function getNewsDetails(id) {
+    return await HttpClientCall({
+        endpoint: `news-details/${id}`,
+        method: "GET",
+        includeAuth: false,
+        data: [],
+    })
+        .then((response) => {
+            return response;
+
+        })
+        .catch((error) => {
+            return [];
+        });
+}
