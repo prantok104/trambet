d6e8074d3fb1e73b58ee6e926811f1792d9f22ae
Hasibul Hasan <hasibulhasan442@gmail.com>
2024-04-28 01:45:27 +0600

    Update Promotion component to fetch data from API and display in table

--- components/Affiliate/Promotions/Promotion.jsx
+++ components/Affiliate/Promotions/Promotion.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import { HttpClientCall } from "@/components/HTTPClient";
+import { notify } from "@/components/Helper";
+import React, { useEffect, useState } from "react";
 
 const Promotion = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    HttpClientCall({
+      endpoint: `affiliate/promotions/1`,
+      method: "GET",
+      includeAuth: true,
+      data: [],
+    }).then((res) => {
+      if (res.status === true) {
+        setData(res?.data?.promotions);
+      } else {
+        notify("error", res.response.data.message);
+      }
+    });
+  }, []);
+
+  console.log(data);
+
   return (
     <div>
       <table className="table table-dark table-striped">
@@ -10,29 +30,35 @@ const Promotion = () => {
             <th scope="col">Title</th>
             <th scope="col">Promo Code</th>
             <th scope="col">Percentage</th>
-            <th scope="col">Create Date</th>
+            <th scope="col">Applied Date</th>
             <th scope="col">Approval</th>
             <th scope="col">Admin Comment</th>
             <th scope="col">Action</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Affiliate Promo Code</td>
-            <td>VNMS6D</td>
-            <td>20%</td>
-            <td>21 Jan, 2024</td>
-            <td>
-              <span class="badge bg-success">Approved</span>
-            </td>
-            <td>
-              <span class="badge bg-warning">No Comment</span>
-            </td>
-            <td>
-              <span class="badge bg-success">Active</span>
-            </td>
-          </tr>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <th scope="row">{++index}</th>
+              <td>{item.title}</td>
+              <td>{item.promo_code}</td>
+              <td>{item.promo_percentage}%</td>
+              <td>{new Date(item.created_at).toLocaleDateString('en-GB')}</td>
+              <td>
+                {item.is_admin_approved == 1 ? (
+                  <span className="badge bg-success">Approved</span>
+                ) : (
+                  <span className="badge bg-warning">Pending</span>
+                )}
+              </td>
+              <td>
+                {item.admin_comment ? item.admin_comment : "No comment"}
+              </td>
+              <td>
+                <span className="badge bg-success">Active</span>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
