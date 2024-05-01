cdf8438a53e84d60b7afe74ada225ee16ac9ca2b
Hasibul Hasan <hasibulhasan442@gmail.com>
2024-05-01 12:55:02 +0600

    Update Register component to fix useEffect dependency and remove console.log statements

--- services/affiliate.jsx
+++ services/affiliate.jsx
@@ -1,6 +1,6 @@
 import { HttpClientCall } from "@/components/HTTPClient";
 
-export async  function getAllPromotions() {
+export async function getAllPromotions() {
     return await HttpClientCall({
       endpoint: "affiliate/promotions",
       method: "GET",
@@ -10,6 +10,21 @@ export async  function getAllPromotions() {
       .then((response) => {
          return response.data;
 
+      })
+      .catch((error) => {
+        return [];
+      });
+  }
+  
+  export async function createPromoCode(data) {
+    return await HttpClientCall({
+      endpoint: "affiliate/promo/create",
+      method: "POST",
+      includeAuth: true,
+      data: data,
+    })
+      .then((response) => {
+        return response;
       })
       .catch((error) => {
         return [];
